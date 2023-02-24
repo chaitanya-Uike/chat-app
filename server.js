62804afd9d6f0ad46099d421cd7f21c0c5c0c683
chaitanya-Uike <chaitanyaguike@gmail.com>
2023-02-24 13:38:13 +0530

    cors added

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
 });
 
+fastify.register(require("@fastify/cors"), {
+  origin: true,
+});
+
 fastify.register(require("fastify-socket.io"));
 
 fastify.register(require("@fastify/auth"));
